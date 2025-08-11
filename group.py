e7d1f5d54fe7e2eef1a01dc65367ef2d9808c06b
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-08-11 22:01:26 +0800

    fix: group msg not being stored by sender

--- group.py
+++ group.py
@@ -294,6 +294,28 @@ def group_message(sock: socket, group_id: str, content: str, app_state: AppState
                     (app_state.peers[member]["ip"], globals.PORT),
                 )
 
+        # Save sent group message to app state
+        if group_id in app_state.owned_groups:
+            group_name = app_state.owned_groups[group_id]["GROUP_NAME"]
+        elif group_id in app_state.joined_groups:
+            group_name = app_state.joined_groups[group_id]["GROUP_NAME"]
+        else:
+            group_name = "Unknown Group"
+        with app_state.lock:
+            if group_id not in app_state.group_messages:
+                app_state.group_messages[group_id] = []
+            app_state.group_messages[group_id].append(
+                {
+                    "from": app_state.user_id,
+                    "group_id": group_id,
+                    "group_name": group_name,
+                    "content": content,
+                    "timestamp": timestamp_now,
+                    "direction": "sent",
+                    "token": message["TOKEN"],
+                }
+            )
+
         if globals.verbose:
             print(f"\n[SEND >]")
             print(f"Message Type : GROUP_MESSAGE")
